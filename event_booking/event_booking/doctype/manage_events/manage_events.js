ff49cd28fe03be50df34e0cb8bdf753fb98a08d6
vinupatil90000 <vinupatil@gmail.com>
2022-12-19 06:48:58 +0000

    second commit

---
+++ event_booking/event_booking/doctype/manage_events/manage_events.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, OPS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Manage Events', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ event_booking/event_booking/doctype/manage_events/test_manage_events.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, OPS and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestManageEvents(FrappeTestCase):
+	pass
